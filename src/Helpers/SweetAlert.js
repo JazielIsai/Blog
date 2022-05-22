0edb515809d42a0ab8354e5c7fa70c89d31640d5
Jaziel Isaí <code.isai.dev@gmail.com>
2022-05-21 23:19:22 -0500

    Update function confirmDialog

--- src/Helpers/SweetAlert.js
+++ src/Helpers/SweetAlert.js
@@ -42,24 +42,29 @@ export function sweetAlert() {
           })
     }
 
-    const confirmDialog = () => {
-        Swal.fire({
-            title: 'Are you sure?',
-            text: "You won't be able to revert this!",
+    async function confirmDialog (title, text, nameAction, actionReady) {
+        let confirm = await Swal.fire({
+            title: title,
+            text: text,
             icon: 'warning',
             showCancelButton: true,
             confirmButtonColor: '#3085d6',
             cancelButtonColor: '#d33',
-            confirmButtonText: 'Yes, delete it!'
+            confirmButtonText: 'Si',
+            cancelButtonText: 'No'
           }).then((result) => {
             if (result.isConfirmed) {
-              Swal.fire(
-                'Deleted!',
-                'Your file has been deleted.',
-                'success'
-              )
+                Swal.fire(
+                    nameAction,
+                    actionReady,
+                    'success'
+                )
+                return true;
+            } else {
+                return false;
             }
           })
+        return confirm;
     }
 
     const errorAlert = () => {
